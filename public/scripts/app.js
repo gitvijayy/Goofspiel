7f9ccd3b209f6b137672afae5f126d1bdaaa72b5
gitvijayy <7822vijay@gmail.com>
2019-05-25 00:45:02 +0000

    gameplay inital implemented

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,6 +14,213 @@ $(document).ready(function () {
     }
   }
   ///////////////////////////////////////////////////////
+
+  const flipGameBoard = () => {
+    $(`.player-1`).empty();
+    $(`.player-2`).empty();
+    $(`.player-1-turn`).empty();
+    $(`.player-2-turn`).empty();
+    $(`.player-1-turn`).empty()
+    $(`.player-2-turn`).empty()
+    $(`main .block-2 article`).removeClass(`hide-display`)
+  }
+  ////////////////////////////////////////////////////
+  const gameNotStarted = (gameId) => {
+    for (var i = 1; i < 14; i++) {
+      if(document.cookie.split(';')[0].split("=")[1] === $(`.bet-1`).text()){
+        $(`.player-1`).append(`<img id ="${i}" src ="images/${i}C.png")>`)
+        $(`.player-2`).append(`<img id ="${i}" src ="images/blackBack")>`)
+
+      }
+      if(document.cookie.split(';')[0].split("=")[1] === $(`.bet-2`).text()){
+      $(`.player-2`).append(`<img id ="${i}" src ="images/${i}H.png")>`)
+      $(`.player-1`).append(`<img id ="${i}" src ="images/blackBack")>`)
+      }
+    }
+    $(`.prize-img`).attr("src", `images/${gameId.split("")[0]}D.png`)
+    $(`.bet-1-img`).attr("src", "images/blackBack")
+    $(`.bet-2-img`).attr("src", "images/blackBack")
+    ///////////////////////////////////////////////////
+    $(`.prize`).data("cardIndex", `${gameId.split("")[0]}`)
+    ////////////////////////////////////////////////
+    $(`.player-1`).data("turn", 1);
+  }
+
+  //////////////!
+  const prizeCardGenerate = (gameId, prizeCard) => {
+    console.log(gameId,prizeCard)
+    $.ajax({
+      type: "PUT",
+      url: `/games/${gameId}`,
+      data: { prize: prizeCard ,type:"prize"},
+      success: () => {
+        // getGameData(document.cookie.split(';')[1].split("=")[1])
+      },
+      error: () => {
+      },
+    });
+  }
+  ///////////////////////////////////////////////
+  const splitTurnsData = (turns,prizeCardCheck) => {
+
+
+
+
+
+    let turnsData = { player1: [], player2: [], player1prize: [], player2prize: [], prize: [] };
+
+    let player1Points = 0;
+    let player2Points = 0;
+    let winnerCheck = 0;
+    $(`.player-1`).empty();
+    $(`.player-2`).empty();
+    $(`.player-1-prize`).empty();
+    $(`.player-2-prize`).empty();
+    turns.forEach((element) => {
+      console.log(element.id)
+      turnsData["player1"].push(element.bet1)
+      turnsData["player2"].push(element.bet2)
+
+     if( element["bet1"] != null) {
+        if (element["winner"] === "player1") {
+          turnsData["player1prize"].push(element.prize)
+        }
+        if (element["winner"] === "player2") {
+          turnsData["player2prize"].push(element.prize)
+        }
+      }
+
+
+
+
+      if (element["winner"] == null && element["bet2"] == null && element["bet1"] != null) {
+        $(`.prize-img`).attr("src", `images/${element["prize"]}D.png`)
+        $(`.prize`).data("cardIndex", `${element["prize"]}`)
+        winnerCheck = 1;
+      }
+
+
+      //! /////////////////////////////////
+
+
+
+
+      //else
+      if (element["bet1"] == null) {
+        $(`.bet-1-img`).attr("src", "images/blackBack")
+        $(`.player-1`).data("turn", 1);
+        $(`.player-2`).data("turn", 0);
+        $(`header .block-1 h1`).text("PLACE A BET")
+        $(`footer .block-1 h1`).text("WAIT")
+        $(`.prize-img`).attr("src", `images/${element["prize"]}D.png`)
+        $(`.prize`).data("cardIndex", `${element["prize"]}`)
+        // $(`.player-2`).attr("turnA", 1);
+      }
+      else if (element["bet2"] == null) {
+        $(`.bet-1-img`).attr("src", `images/${element["bet1"]}C.png`)
+        $(`.player-2`).data("turn", 1);
+        $(`.player-1`).data("turn", 0);
+        $(`header .block-1 h1`).text("WAIT")
+        $(`footer .block-1 h1`).text("PLACE A BET")
+        // $(`.player-2`).attr("turnA", 1);
+      } else {
+        turnsData["prize"].push(element.prize)
+        $(`.player-1`).data("turn", 1);
+        $(`.player-2`).data("turn", 0);
+        //$(`.player-1`).attr("turnA", 1);
+        $(`.bet-1-img`).attr("src", "images/blackBack")
+        $(`.bet-2-img`).attr("src", "images/blackBack")
+        $(`header .block-1 h1`).text("PLACE A BET")
+        $(`footer .block-1 h1`).text("WAIT")
+
+      }
+
+
+
+      ////////////////////////////////////////////
+    })
+    for (var i = 1; i < 14; i++) {
+      if (!turnsData.player1.includes(i) && document.cookie.split(';')[0].split("=")[1] === $(`.bet-1`).text()) {
+
+       // if(){
+         // $(`.player-1`).append(`<img id ="${i}" src ="images/${i}C.png")>`)
+
+
+       // }
+        //if(document.cookie.split(';')[0].split("=")[1] === (`.bet-2`).text()){
+       // $(`.player-2`).append(`<img id ="${i}" src ="images/${i}H.png")>`)
+
+        //}
+
+
+
+        $(`.player-1`).append(`<img id ="${i}" src ="images/${i}C.png")>`)
+        $(`.player-2`).append(`<img id ="${i}" src ="images/blackBack")>`)
+      }
+      if (!turnsData.player2.includes(i) && document.cookie.split(';')[0].split("=")[1] === $(`.bet-2`).text()) {
+        $(`.player-2`).append(`<img id ="${i}" src ="images/${i}H.png")>`)
+        $(`.player-1`).append(`<img id ="${i}" src ="images/blackBack")>`)
+      }
+      if (i <= turnsData.player1prize.length) {
+        $(`.player-1-prize`).append(`<img id ="${turnsData.player1prize[i - 1]}" src ="images/${turnsData.player1prize[i - 1]}D.png")>`)
+        player1Points += turnsData.player1prize[i - 1]
+      }
+      if (i <= turnsData.player2prize.length) {
+        $(`.player-2-prize`).append(`<img id ="${turnsData.player2prize[i - 1]}" src ="images/${turnsData.player2prize[i - 1]}D.png")>`)
+        player2Points += turnsData.player2prize[i - 1]
+      }
+    }
+
+// if (element["bet2"] == null && element["bet"] == null) {
+        if (prizeCardCheck === 1) {
+          let checker = 0;
+          while (checker === 0) {
+            let prizeCard = Math.floor(Math.random() * 14);
+            if (prizeCard === 0) { prizeCard += 1 }
+            if (!turnsData["prize"].includes(prizeCard)) {
+              $(`.prize-img`).attr("src", `images/${prizeCard}D.png`)
+              $(`.prize`).data("cardIndex", prizeCard)
+              prizeCardGenerate(document.cookie.split(';')[1].split("=")[1], prizeCard);
+              checker = 1;
+              prizeCardCheck=0;
+            }
+          }
+        }
+      // }
+
+
+
+
+    $(`header .block-2 p`).text(`Total Points - ${player1Points}`)
+    $(`footer .block-2 p`).text(`Total Points - ${player2Points}`)
+
+    return turnsData;
+  }
+
+  ///////////////////////////////////////////
+  const getGameData = (gameId,prizeCardCheck) => {
+
+
+
+    $.ajax({
+      method: "GET",
+      url: `games/${gameId}`
+    }).done((turns) => {
+
+      //////////////!
+      console.log(turns)
+      if (!turns.length) {
+        gameNotStarted(gameId);
+      } else {
+        const gameData = splitTurnsData(turns,prizeCardCheck)
+        console.log(gameData)
+      }
+    })
+  }
+
+
+
+  /////////////////////////////////////////////////////////////
   const getUserInfo = (user) => {
     $.ajax({
       method: "GET",
@@ -60,119 +267,14 @@ $(document).ready(function () {
       data: { username: user },
       success: () => {
         getUserInfo(user)
+
       },
       error: () => {
       },
     });
   }
+
   /////////////////////////////////////////////////////////
-  const flipGameBoard = () => {
-    $(`.player-1`).empty();
-    $(`.player-2`).empty();
-    $(`.player-1-turn`).empty();
-    $(`.player-2-turn`).empty();
-    $(`.player-1-turn`).empty()
-    $(`.player-2-turn`).empty()
-    $(`main .block-2 article`).removeClass(`hide-display`)
-  }
-  ////////////////////////////////////////////////////
-  const gameNotStarted = (gameId) => {
-    for (var i = 1; i < 14; i++) {
-      $(`.player-1`).append(`<img id ="${i}" src ="images/${i}C.png")>`)
-      $(`.player-2`).append(`<img id ="${i}" src ="images/${i}H.png")>`)
-    }
-    $(`.prize-img`).attr("src", `images/${gameId.split("")[0]}D.png`)
-    $(`.bet-1-img`).attr("src", "images/blackBack")
-    $(`.bet-2-img`).attr("src", "images/blackBack")
-    ///////////////////////////////////////////////////
-    $(`.prize`).data("cardIndex", `${gameId.split("")[0]}`)
-    ////////////////////////////////////////////////
-    $(`.player-1`).data("turn", 1);
-  }
-  const splitTurnsData = (turns) => {
-    let turnsData = { player1: [], player2: [], player1prize: [], player2prize: [], prize: [] };
-    let player1Points = 0;
-    let player2Points = 0;
-    let winnerCheck = 0;
-    $(`.player-1`).empty();
-    $(`.player-2`).empty();
-    $(`.player-1-prize`).empty();
-    $(`.player-2-prize`).empty();
-    turns.forEach((element) => {
-      console.log(element.id)
-      turnsData["player1"].push(element.bet1)
-      turnsData["player2"].push(element.bet2)
-      if (element["winner"] === "player1") {
-        turnsData["player1prize"].push(element.prize)
-      }
-      if (element["winner"] === "player2") {
-        turnsData["player2prize"].push(element.prize)
-      }
-      if (element["winner"] == null && element["bet2"] == null) {
-        $(`.prize-img`).attr("src", `images/${element["prize"]}D.png`)
-        $(`.prize`).data("cardIndex", `${element["prize"]}`)
-        winnerCheck = 1;
-      }
-      if (element["bet2"] == null) {
-        $(`.bet-1-img`).attr("src", `images/${element["bet1"]}C.png`)
-        $(`.player-2`).data("turn", 1);
-        $(`.player-2`).attr("turnA", 1);
-      } else {
-        turnsData["prize"].push(element.prize)
-        $(`.player-1`).data("turn", 1);
-        $(`.player-1`).attr("turnA", 1);
-        $(`.bet-1-img`).attr("src", "images/blackBack")
-        $(`.bet-2-img`).attr("src", "images/blackBack")
-      }
-    })
-    for (var i = 1; i < 14; i++) {
-      if (!turnsData.player1.includes(i)) {
-        $(`.player-1`).append(`<img id ="${i}" src ="images/${i}C.png")>`)
-      }
-      if (!turnsData.player2.includes(i)) {
-        $(`.player-2`).append(`<img id ="${i}" src ="images/${i}H.png")>`)
-      }
-      if (i <= turnsData.player1prize.length) {
-        $(`.player-1-prize`).append(`<img id ="${turnsData.player1prize[i - 1]}" src ="images/${turnsData.player1prize[i - 1]}D.png")>`)
-        player1Points += turnsData.player1prize[i - 1]
-      }
-      if (i <= turnsData.player2prize.length) {
-        $(`.player-2-prize`).append(`<img id ="${turnsData.player2prize[i - 1]}" src ="images/${turnsData.player2prize[i - 1]}D.png")>`)
-        player2Points += turnsData.player2prize[i - 1]
-      }
-    }
-    if (winnerCheck === 0) {
-      let checker = 0;
-      while (checker === 0) {
-        let prizeCard = Math.floor(Math.random() * 14);
-        if (prizeCard === 0) { prizeCard += 1 }
-        if (!turnsData["prize"].includes(prizeCard)) {
-          $(`.prize-img`).attr("src", `images/${prizeCard}D.png`)
-          $(`.prize`).data("cardIndex", prizeCard)
-          checker = 1;
-        }
-      }
-    }
-    $(`header .block-2 p`).text(`Total Points - ${player1Points}`)
-    $(`footer .block-2 p`).text(`Total Points - ${player2Points}`)
-    return turnsData;
-  }
-  ///////////////////////////////////////////
-  const getGameData = (gameId) => {
-    $.ajax({
-      method: "GET",
-      url: `games/${gameId}`
-    }).done((turns) => {
-      console.log(turns)
-      if (!turns.length) {
-        gameNotStarted(gameId);
-      } else {
-        const gameData = splitTurnsData(turns)
-        console.log(gameData)
-      }
-    })
-  }
-  /////////////////////////////////////////////////////////////
   $(document).on(`click`, `.user-submit`, function () {
     if ($(`.username`).val().trim()) {
       addNewUser($(`.username`).val())
@@ -183,12 +285,14 @@ $(document).ready(function () {
   //////////////////////////////////////////////////////////////
   $(document).on(`click`, `.new-game`, function () {
     newGame(document.cookie.split(';')[0].split("=")[1])
+
   })
   ///////////////////////////////////////////////////////////////////
   $(document).on(`click`, `main .block-1 p`, function () {
     let player1 = $(this).data("player1");
     let player2 = $(this).data("player2");
     if (player1 == null || player2 == null) {
+      //////////////!
       alert("Waiting for a player to join the game")
       return false;
     }
@@ -202,22 +306,30 @@ $(document).ready(function () {
   /////////////////////////////////////////////////////////
   $(document).on(`click`, `.player-1 img`, function () {
     if (document.cookie.split(';')[0].split("=")[1] != ($(`.bet-1`).text())) {
+      //////////////!
       alert("Am Watching");
       return;
     }
     let turnCheck = $(`.player-1`).data("turn");
     //console.log(turnCheck)
     if (turnCheck === 1) {
+      // $(`.player-1`).data("turn", 0);
+      // $(`.player-2`).data("turn", 1);
+      //////////////!
       $.ajax({
         type: "PUT",
         url: `/games/${document.cookie.split(';')[1].split("=")[1]}`,
-        data: { prize: $(`.prize`).data("cardIndex"), bet: $(this).attr("id") },
+        data: { prize: $(`.prize`).data("cardIndex"), bet: $(this).attr("id"),type:"bet1" },
         success: () => {
-          getGameData(document.cookie.split(';')[1].split("=")[1])
+
+          getGameData(document.cookie.split(';')[1].split("=")[1],0)
+
+
         },
         error: () => {
         },
       });
+
     } else {
       alert("Wait")
     }
@@ -229,14 +341,19 @@ $(document).ready(function () {
     }
     let turnCheck = $(`.player-2`).data("turn");
     if (turnCheck === 1) {
-      $(`.player-2`).data("turn", 0);
-      $(`.player-1`).data("turn", 1);
+      // $(`.player-2`).data("turn", 0);
+      // $(`.player-1`).data("turn", 1);
       $.ajax({
         type: "PUT",
         url: `/games/${document.cookie.split(';')[1].split("=")[1]}`,
-        data: { prize: $(`.prize`).data("cardIndex"), bet: $(this).attr("id") },
+        data: { prize: $(`.prize`).data("cardIndex"), bet: $(this).attr("id"),type:"bet2" },
         success: () => {
-          getGameData(document.cookie.split(';')[1].split("=")[1])
+
+
+
+
+          getGameData(document.cookie.split(';')[1].split("=")[1],1)
+
         },
         error: () => {
         },
@@ -245,6 +362,7 @@ $(document).ready(function () {
       alert("Wait")
     }
   });
+
   loginCheck();
   getUserInfo(document.cookie.split(';')[0].split("=")[1])
 })
--- routes/games.js
+++ routes/games.js
@@ -1,38 +1,38 @@
 'use strict';
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
-//on put request to /games check if any games missing player2, and if user already in a game waiting for player2
-//if game found waiting for player2 where player1 is not user, update database with player2
-//if game not found with missing player2 or if only game missing player2 is current user's game, create another new game
+  //on put request to /games check if any games missing player2, and if user already in a game waiting for player2
+  //if game found waiting for player2 where player1 is not user, update database with player2
+  //if game not found with missing player2 or if only game missing player2 is current user's game, create another new game
   router.put('/', (req, res) => {
-      knex
+    knex
       .select('id')
       .from('games')
       .whereNull('player2')
       .where('player1', '!=', req.body.username)
       .then((results) => {
-        if(results[0]){
+        if (results[0]) {
           knex('games')
-          .where('id', results[0].id)
-          .update({player2: req.body.username, status:'active'})
-          .then(res.status(200).send());
+            .where('id', results[0].id)
+            .update({ player2: req.body.username, status: 'active' })
+            .then(res.status(200).send());
         } else {
           knex('games')
-          .insert({player1: req.body.username, status: 'inactive'})
-          .then(res.status(200).send());
+            .insert({ player1: req.body.username, status: 'inactive' })
+            .then(res.status(200).send());
         }
       })
       .catch(
-        function(error) {
-          res.status(500).json({error});
+        function (error) {
+          res.status(500).json({ error });
         }
       );
   });
 
-//on get request to /games/:gameid - retrieve all turn info related to the game
+  //on get request to /games/:gameid - retrieve all turn info related to the game
   router.get('/:gameid', (req, res) => {
     knex
       .select('*')
@@ -40,99 +40,177 @@ module.exports = (knex) => {
       .where('games_id', '=', req.params.gameid)
       .then((results) => {
         res.json(results);
-    });
+      });
   });
 
-//on put request to /games/:gameid - enter new information into turns
-//Find all turns with gameid, if there's an entry with no bet2, that turn's unfinished, put bet into bet2
-//After bet2 is updated with new value, turn is finished - calculate winner & points won
+  //on put request to /games/:gameid - enter new information into turns
+  //Find all turns with gameid, if there's an entry with no bet2, that turn's unfinished, put bet into bet2
+  //After bet2 is updated with new value, turn is finished - calculate winner & points won
   router.put('/:gameid', (req, res) => {
     //if game status = done
-     if (req.body.status === 'done'){
-    //   // select all turns that matches game id
-    //   knex('turns')
-    //     .select('points')
-    //     .where('games_id', req.params.gameid)
-    //     // select where winner = player1 and count the total points compare value, then select winner with id and player1/2 from games, update status, winner
-    //     .where('winner', 'player1')
-    //     .sum('points')
-    //     .then((results) => {
-    //       let p1 = results[0].points;
-    //       // select where winner = player 2 and count the total points
-    //       knex('turns')
-    //         .select('points')
-    //         .where('games_id', req.params.gameid)
-    //         .sum('points')
-    //         //compare who has more points
-    //         .then((results) =>{
-    //           let p2 = results[0].points;
-    //           if (p1>p2){
-    //             knex('games')
-    //               .select('player1')
-    //               .where('id', req.params.gameid)
-    //               .then((results) =>{
-    //                 knex('games')
-    //                   .where('id', req.params.gameid)
-    //                   .update('winner', results[0].player1)
+    if (req.body.status === 'done') {
+      //   // select all turns that matches game id
+      //   knex('turns')
+      //     .select('points')
+      //     .where('games_id', req.params.gameid)
+      //     // select where winner = player1 and count the total points compare value, then select winner with id and player1/2 from games, update status, winner
+      //     .where('winner', 'player1')
+      //     .sum('points')
+      //     .then((results) => {
+      //       let p1 = results[0].points;
+      //       // select where winner = player 2 and count the total points
+      //       knex('turns')
+      //         .select('points')
+      //         .where('games_id', req.params.gameid)
+      //         .sum('points')
+      //         //compare who has more points
+      //         .then((results) =>{
+      //           let p2 = results[0].points;
+      //           if (p1>p2){
+      //             knex('games')
+      //               .select('player1')
+      //               .where('id', req.params.gameid)
+      //               .then((results) =>{
+      //                 knex('games')
+      //                   .where('id', req.params.gameid)
+      //                   .update('winner', results[0].player1)
 
-    //               })
+      //               })
 
-    //           }if (p2>p1) {
+      //           }if (p2>p1) {
 
-    //           } else {
-    //             // ties
-    //           }
+      //           } else {
+      //             // ties
+      //           }
 
 
-    //         })
-    //     })
+      //         })
+      //     })
 
 
       knex('games')
-      .where('id', req.params.gameid)
-      .update({'status':'done'})
-      .then(
-        res.status(200).send())
-      .catch(
-        function(error) {
-          res.status(500).json({error});
-        }
-      );
-    // else games is not over, update each turn
+        .where('id', req.params.gameid)
+        .update({ 'status': 'done' })
+        .then(
+          res.status(200).send())
+        .catch(
+          function (error) {
+            res.status(500).json({ error });
+          }
+        );
+      // else games is not over, update each turn
     } else {
       knex('turns')
-        .select('id', 'bet1', 'prize')
+        .select('id',  'prize')
         .where('games_id', req.params.gameid)
         .whereNull('bet2')
         .then((results) => {
           //logic check to see who wins
-          if(results[0]){
-            // let score = results[0].bet1 - req.body.bet;
-            let winner = '';
-            let points = results[0].prize;
-            if (results[0].bet1 > req.body.bet){
-              winner = 'player1'
-            } else if (results[0].bet1 < req.body.bet) {
-              winner = 'player2'
-            } else {
-              winner = null;
-              points = 0;
+          if (results[0]) {
+            //   // let score = results[0].bet1 - req.body.bet;
+            //   let winner = '';
+            //   let points = results[0].prize;
+            //   if (results[0].bet1 > req.body.bet){
+            //     winner = 'player1'
+            //   } else if (results[0].bet1 < req.body.bet) {
+            //     winner = 'player2'
+            //   } else {
+            //     winner = null;
+            //     points = 0;
+            //   }
+            //   //update database with bet2, winner, prize
+            //   knex('turns')
+            //     .where('id', results[0].id)
+            //     .update({'bet2':req.body.bet, 'winner':winner, 'points':points})
+            //     .then(res.status(200).send());
+            // } else {
+            //   knex('turns')
+            //     .where('id', results)
+            //     .insert({games_id:req.params.gameid, prize:req.body.prize, bet1:req.body.bet})
+            //     .then(res.status(200).send());
+            // }
+
+
+            if (req.body.type === "bet1") {
+              knex('turns')
+                .where('id', results[0].id)
+                .update({ 'bet1': req.body.bet })
+                .then(res.status(200).send());
+            } else if (req.body.type === "bet2") {
+              let winner = '';
+              let points = results[0].prize;
+              if (results[0].bet1 > req.body.bet) {
+                winner = 'player1'
+              } else if (results[0].bet1 < req.body.bet) {
+                winner = 'player2'
+              } else {
+                winner = null;
+                points = 0;
+              }
+              //update database with bet2, winner, prize
+              knex('turns')
+                .where('id', results[0].id)
+                .update({ 'bet2': req.body.bet, 'winner': winner, 'points': points })
+                .then(res.status(200).send());
             }
-            //update database with bet2, winner, prize
-            knex('turns')
-              .where('id', results[0].id)
-              .update({'bet2':req.body.bet, 'winner':winner, 'points':points})
-              .then(res.status(200).send());
+
           } else {
             knex('turns')
               .where('id', results)
-              .insert({games_id:req.params.gameid, prize:req.body.prize, bet1:req.body.bet})
+              .insert({ 'bet1': req.body.bet ,games_id: req.params.gameid, prize: req.body.prize })
               .then(res.status(200).send());
           }
+
+          // if(results[0]){
+          //   // let score = results[0].bet1 - req.body.bet;
+          //   let winner = '';
+          //   let points = results[0].prize;
+          //   if (results[0].bet1 > req.body.bet){
+          //     winner = 'player1'
+          //   } else if (results[0].bet1 < req.body.bet) {
+          //     winner = 'player2'
+          //   } else {
+          //     winner = null;
+          //     points = 0;
+          //   }
+          //   //update database with bet2, winner, prize
+          //   knex('turns')
+          //     .where('id', results[0].id)
+          //     .update({'bet2':req.body.bet, 'winner':winner, 'points':points})
+          //     .then(res.status(200).send());
+          // } else {
+          //   knex('turns')
+          //     .where('id', results)
+          //     .insert({games_id:req.params.gameid, prize:req.body.prize, bet1:req.body.bet})
+          //     .then(res.status(200).send());
+          // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         })
         .catch(
-          function(error) {
-            res.status(500).json({error});
+          function (error) {
+            res.status(500).json({ error });
           }
         );
     }
